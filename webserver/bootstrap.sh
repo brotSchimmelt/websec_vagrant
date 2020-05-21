c0a8d8e38a04f293c0d18012a7d0fc2aa3ba6db7
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-05-21 22:21:07 +0200

    enable docker on VM

--- webserver/bootstrap.sh
+++ webserver/bootstrap.sh
@@ -11,28 +11,42 @@ apt-get -y install mc
 echo "create new user ..."
 useradd -m -s /bin/bash -G sudo,adm,cdrom,sudo,dip,plugdev,lxd websec
 echo websec:websec | chpasswd
-# passwd -e websec
+# passwd -e websec # websec needs to change password on first login
 
+# # only needed if apache should run directly in the vm
+# echo "install webserver ..."
+# apt-get -y install apache2 php libapache2-mod-php mysql-server php-mysql
+# rm /var/www/html/index.html
+# mv /home/vagrant/websec /var/www/html/websec
+# chown www-data /var/www/html/websec/databases
 
-echo "install webserver ..."
-apt-get -y install apache2 php libapache2-mod-php mysql-server php-mysql
+echo "install docker ..."
+DEBIAN_FRONTEND=noninteractive apt-get install apt-transport-https ca-certificates curl software-properties-common -yq
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install docker-ce -yq
+systemctl start docker
+systemctl enable docker
 
-rm /var/www/html/index.html
+echo "wait for docker ..."
+sleep 30
 
-# echo "install docker ..."
-# apt-get install apt-transport-https ca-certificates curl software-properties-common
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# apt-get update
-# apt-get install docker-ce
-# systemctl start docker
-# systemctl enable docker
+echo "add user to group docker ..."
+groupadd docker
+usermod -aG docker websec
 
+echo "instal docker compose ..."
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+echo "copy files for webserver ..."
 cp -r /home/vagrant/websec /home/websec/websec_backup
-mv /home/vagrant/websec /var/www/html/websec
-chown www-data /var/www/html/websec/databases
+cp -r /home/vagrant/docker /home/websec/docker_backup
+mv /home/vagrant/docker /home/websec/docker
+mv /home/vagrant/websec /home/websec/docker/www
 
-# copy authorized keys to new user
+echo "copy authorized keys to new user ..."
 sudo -i
 mkdir /home/websec/.ssh/
 cp /home/vagrant/.ssh/authorized_keys /home/websec/.ssh/authorized_keys
@@ -41,7 +55,6 @@ chown websec:websec /home/websec/.ssh/authorized_keys
 
 touch /etc/motd
 echo "
-
  _    _  ____  ____  ___  ____  ___ 
 ( \/\/ )( ___)(  _ \/ __)( ___)/ __)
  )    (  )__)  ) _ <\__ \ )__)( (__ 
@@ -51,6 +64,10 @@ echo "
 
 Remember to change passwords for vagrant ('vagrant'),
 root ('vagrant') and websec ('websec') as soon as possible!
+
+--> Start the webserver with:
+1. $ cd docker
+2. $ docker-compose up -d
 
 
 " >> /etc/motd
