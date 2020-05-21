e35c90d16f7c55aea62fb2ee1bf13c3ec05eb4dc
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-05-21 23:19:26 +0200

    rename project folder

--- install_server_win.ps1
+++ install_server_win.ps1
@@ -1,5 +1,5 @@
 # Path to the vagrant folder
-$path = ".\webserver"
+$path = ".\websec"
 # hyper-v specific vagrantfile (default)
 $vagrantfile = ".\Vagrantfiles\Vagrantfile_hyperv"
 # vagrantfile with ssh user
